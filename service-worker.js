8b9b6263fd0da45204fa495e712d9087021b28d9
munatsitafadzwa1-ship-it <munatsi.tafadzwa1@gmail.com>
2026-08-19 08:53:39 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'through-the-word-v1';
+const CACHE_NAME = 'through-the-word-v2';
 const ASSETS = ['./index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', (event) => {
